8d1a92c60952e35586a57c55263b8307e717265c
Baris Yilmaz <baris.yilmaz@inventanalytics.com>
2023-03-13 17:56:21 +0300

    antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from 'antd';
+
 function App() {
-  return <div>selamun aleyke </div>;
+  return (
+    <>
+      <div>selamun aleyke </div>
+      <Button> merhabalar aq</Button>
+    </>
+  );
 }
 
 export default App;
